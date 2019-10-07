f4a3826f36632f30c3a8aac4d887dc56449b54c3
Andres Carranza <diacarry@gmail.com>
2019-10-07 16:33:52 -0500

    Creacion de Diccionario (es/files.php)

--- resources/lang/es/files.php
+++ resources/lang/es/files.php
@@ -1,10 +1,61 @@
 <?php
 
 return [
+    'menu' => 'Menú',
+    'account' => 'Cuenta',
+    'previous' => 'Compañias',
+    'previousB' => 'Empleados',
+    'partner' => 'Compañeros',
+
+    'warningA' => 'NO HAY COMPAÑIAS REGISTRADAS',
+    'warningB' => 'NO HAY EMPLEADOS REGISTRADOS EN LA COMPAÑIA',
+    'warningC' => 'NO HAY EMPLEADOS REGISTRADOS',
+
     'menuTitle' => 'Gestión del personal y empresas',
     'menuLinkA' => 'Compañias',
     'menuLinkB' => 'Empleados',
 
-    'homeWelcomeA' => 'BIENVENIDO... Estas logueado!',
-    'homeWelcomeB' => 'Elija un lenguage',
+    'homeWelcomeA' => 'BIENVENIDO... Te encuentras logueado!',
+    'homeWelcomeB' => 'Elija un idioma',
+
+    'generalButtonRegister' => 'Registrar',
+    'generalButtonUpdate' => 'Modificar',
+    'selectEmpty' => 'Elija una opcion',
+
+
+    'tableEmail' => 'Correo',
+    'tableName' => 'Nombre',
+    'tableWebsite' => 'Sitio Web',
+    'tableEmployees' => 'Empleados',
+    'tableFirstName' => 'Nombres',
+    'tableLastName' => 'Apellidos',
+    'tablePhone' => 'Telefono',
+    'tableCompany' => 'Compañia',
+
+    'tableButtonEdit' => 'Editar',
+    'tableButtonDelete' => 'Eliminar',
+
+    'companyTitleA' => 'Compañias',
+    'companyTitleB' => 'Listado de Compañias',
+    'companyTitleC' => 'Formulario para añadir una nueva compañia',
+    'companyTitleD' => ' - Listado de empleados',
+    'companyTitleE' => 'Formulario para editar datos de una compañia',
+    'companyFormLabelA' => 'Correo de la compañia',
+    'companyFormLabelB' => 'Nombre de la compañia',
+    'companyFormLabelC' => 'Direccion electrónica',
+    'companyFormLabelD' => 'Añada un logo a la compañia',
+    'companyButtonCreate' => 'Crear una nueva comañia',
+
+    
+    'employeeTitleA' => 'Empleados',
+    'employeeTitleB' => 'Listado de empleados ',
+    'employeeTitleC' => 'Formulario para añadir un nuevo empleado',
+    'employyeTitleD' => 'Formulario para editar información de un empleado',
+
+    'employeeFormLabelA' => 'Compañia',
+    'employeeFormLabelB' => 'Correo de usuario',
+    'employeeFormLabelC' => 'Nombres',
+    'employeeFormLabelD' => 'Apellidos',
+    'employeeFormLabelE' => 'Telefono',
+    'employeeButtonCreate' => 'Creaar un nuevo Empleado'
 ];
